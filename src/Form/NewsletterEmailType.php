f2e0a7b6ce0dea07652b021325f70499aeaa3b6d
Hugimo-kun <hugo.simoneau@gmail.com>
2024-06-17 09:25:57 +0200

    few minor front updates on the login form

--- src/Form/NewsletterEmailType.php
+++ src/Form/NewsletterEmailType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\NewsletterEmail;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +14,12 @@ class NewsletterEmailType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('Inscription', SubmitType::class)
+            ->add('email', EmailType::class, [
+                'label_attr' => ["class" => "text-2xl"],
+            ])
+            ->add('Inscription', SubmitType::class, [
+                'attr' => ['class' => 'text-2xl p-1 bg-sky-600 rounded hover:bg-sky-700 text-white mx-auto my-5 block'],
+            ])
         ;
     }
 
